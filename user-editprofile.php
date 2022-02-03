4ffd42b575edfcbd21e707a0445ab907cd0d0e24
Nandita patel <78089239+Nandita2110@users.noreply.github.com>
2022-02-03 14:12:54 +0530

    updates

--- user-editprofile.php
+++ user-editprofile.php
@@ -9,10 +9,19 @@ include_once "scripts/DBUser.php";
 //     exit();
 // }
 
+
 $user_email = $_SESSION['email'];
 // $user_id=  $_SESSION['username']->id;
-$sql= "SELECT * FROM usertable WHERE email= $user_email" ;
-$user = DBUser::query($sql)->fetchAll(PDO::FETCH_OBJ);
+// $sql= "SELECT * FROM usertable WHERE email= $user_email O*--
+
+$stmt = DBUser::query(
+    "SELECT * FROM usertable WHERE email=?",
+    [$user_email]
+);
+$user = $stmt->fetch(PDO::FETCH_OBJ);
+
+
+
 
 // $cities = ["Ahmednagar", "Akola", "Akot", "Amalner", "Ambejogai", "Amravati", "Anjangaon", "Arvi", "Aurangabad", "Bhiwandi", "Dhule", "Kalyan-Dombivali", "Ichalkaranji", "Kalyan-Dombivali", "Karjat", "Latur", "Loha", "Lonar", "Lonavla", "Mahad", "Malegaon", "Malkapur", "Mangalvedhe", "Mangrulpir", "Manjlegaon", "Manmad", "Manwath", "Mehkar", "Mhaswad", "Mira-Bhayandar", "Morshi", "Mukhed", "Mul", "Greater Mumbai*", "Murtijapur", "Nagpur", "Nanded-Waghala", "Nandgaon", "Nandura", "Nandurbar", "Narkhed", "Nashik", "Navi Mumbai", "Nawapur", "Nilanga", "Osmanabad", "Ozar", "Pachora", "Paithan", "Palghar", "Pandharkaoda", "Pandharpur", "Panvel", "Parbhani", "Parli", "Partur", "Pathardi", "Pathri", "Patur", "Pauni", "Pen", "Phaltan", "Pulgaon", "Pune", "Purna", "Pusad", "Rahuri", "Rajura", "Ramtek", "Ratnagiri", "Raver", "Risod", "Sailu", "Sangamner", "Sangli", "Sangole", "Sasvad", "Satana", "Satara", "Savner", "Sawantwadi", "Shahade", "Shegaon", "Shendurjana", "Shirdi", "Shirpur-Warwade", "Shirur", "Shrigonda", "Shrirampur", "Sillod", "Sinnar", "Solapur", "Soyagaon", "Talegaon Dabhade", "Talode", "Tasgaon", "Thane", "Tirora", "Tuljapur", "Tumsar", "Uchgaon", "Udgir", "Umarga", "Umarkhed", "Umred", "Uran", "Uran Islampur", "Vadgaon Kasba", "Vaijapur", "Vasai-Virar", "Vita", "Wadgaon Road", "Wai", "Wani", "Wardha", "Warora", "Warud", "Washim", "Yavatmal", "Yawal", "Yevla"];
 $cities = ["Ahmedabad", "Amreli", "Anand", "Bhuj", "Bopal", "Dahod" , "Godhra", "Kadi", "Kalol", "Kheda", "Mandvi", "Navsari", "Rajkot", "Vadodara"];
@@ -42,34 +51,34 @@ $cities = ["Ahmedabad", "Amreli", "Anand", "Bhuj", "Bopal", "Dahod" , "Godhra", 
 
             <form action="scripts/updatehall.php" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label for="">Name <?php echo $user_email ?></label>
-                    <input value="<?= $provider->name;  ?>" id="name" name="name" type="text" class="form-control" placeholder="Name" required>
+                    <label for="">Name <?php echo $user->id ?></label>
+                    <input value="<?= $user->name;  ?>" id="name" name="name" type="text" class="form-control" placeholder="Name" required>
                 </div>
 
                 <div class="form-group">
                     <label for="">Contact No.</label>
-                    <input value="<?= $provider->contact; ?>" id="contact" name="contact" type="text" class="form-control" placeholder="Contact" minlength="10" maxlength="10" required>
+                    <input value="<?= $user->contact; ?>" id="contact" name="contact" type="text" class="form-control" placeholder="Contact" minlength="10" maxlength="10" required>
                 </div>
 
                 <div class="form-group">
                     <label for="">Email</label>
-                    <input value="<?= $provider->email; ?>" id="email" name="email" type="email" class="form-control" placeholder="Email" required>
+                    <input value="<?= $user->email; ?>" id="email" name="email" type="email" class="form-control" placeholder="Email" required>
                 </div>
 
                 <div class="form-group">
                     <label for="">Address Line 1</label>
-                    <input value="<?= $provider->adder1; ?>" id="adder1" name="adder1" type="text" class="form-control" placeholder="Enter Address line-1" required>
+                    <input value="<?= $user->address; ?>" id="adder1" name="adder1" type="text" class="form-control" placeholder="Enter Address line-1" required>
                 </div>
 
-                <div class="form-group">
+                <!-- <div class="form-group">
                     <label for="">Address
                         Line 2</label>
                     <input value="<?= $provider->adder2; ?>" id="adder2" name="adder2" type="text" class="form-control" placeholder="Enter Address line-2" required>
-                </div>
+                </div> -->
 
                 <div class="form-group">
                     <label for="">City</label>
-                    <select value="<?= $provider->city; ?>" class="form-control" name="city" id="city">
+                    <select value="<?= $user->city; ?>" class="form-control" name="city" id="city">
                         <?php foreach ($cities as $city) : ?>
                             <option value="<?= $city ?>"> <?= $city ?>
                             </option>
@@ -80,7 +89,7 @@ $cities = ["Ahmedabad", "Amreli", "Anand", "Bhuj", "Bopal", "Dahod" , "Godhra", 
                 <div class="form-group">
                     <div class="row">
                         <div class="col-2 text-center">
-                            <img style=" height: 100px;" src="images/<?= $provider->photo; ?>">
+                            <img style=" height: 100px;" src="images/<?= $user->photo; ?>">
                             <div class="text-center">Old Photo</div>
                         </div>
                         <div class="col">
@@ -90,24 +99,10 @@ $cities = ["Ahmedabad", "Amreli", "Anand", "Bhuj", "Bopal", "Dahod" , "Godhra", 
                     </div>
                 </div>
 
-                <div class="form-group" style="margin-top: 15px;">
-                    <label for="">Description</label>
-                    <textarea name="descr" id="descr" class="form-control" cols="30" rows="5" placeholder="Add Discription About Your Hall" required><?= $provider->descr; ?></textarea>
-                </div>
-
-                <div class="form-group">
+                <!-- <div class="form-group">
                     <label for="">Password</label>
-                    <input value="<?= $provider->password; ?>" id="password" name="password" type="password" class="form-control" placeholder="Enter 4 Charectar Password" minlength="4" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="">Profession</label>
-                    <select class="form-control" name="profession" id="profession">
-                        <option value="Electrician">Electrician</option>
-                        <option value="Plumber">Plumber</option>
-                        <option value="Mobile">Mobile Repairer</option>
-                    </select>
-                </div>
+                    <input value="<?= $user->password; ?>" id="password" name="password" type="password" class="form-control" placeholder="Enter 4 Charectar Password" minlength="4" required>
+                </div> -->
 
                 <button style="margin-top: 20px;" class="btn btn-success btn-block" type="submit" name="register" id="register">Update</button>
             </form>
