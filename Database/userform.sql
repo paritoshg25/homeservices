28ca2cd75ff1759cc0194934217972eb35b7f9c4
Nandita patel <78089239+Nandita2110@users.noreply.github.com>
2022-02-04 15:00:29 +0530

    updates

--- Database/userform.sql
+++ Database/userform.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 06:07 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Feb 04, 2022 at 10:28 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,6 +31,10 @@ CREATE TABLE `usertable` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `code` mediumint(50) NOT NULL,
   `status` text NOT NULL
@@ -40,13 +44,13 @@ CREATE TABLE `usertable` (
 -- Dumping data for table `usertable`
 --
 
-INSERT INTO `usertable` (`id`, `name`, `email`, `password`, `code`, `status`) VALUES
-(1, 'admin', 'admin.servify.24x7@gmail.com', 'admin123', 481913, 'verified'),
-(5, 'Paritosh', 'shivgaming007@gmail.com', '$2y$10$h1K1kECGvmtIKRsJv6Ucx.3RUjUEdMkd1l/t5WbWtf58pBAH5Vc0i', 0, 'verified'),
-(6, 'Suraj Mehta', 'mehtasuraj7984@gmail.com', '$2y$10$sEsUilAaPchVY5/ETSWwJOvrVTEI6LtMbXbfazIqL/r44OC3N2KU.', 564458, 'verified'),
-(13, 'Tirth', 'paritoshg84@gmail.com', '$2y$10$knbNw5aY2JKsKDDWFtTwrOWYZ/5jAE.0r20OrMv0ncYfzIzfmd/pu', 0, 'verified'),
-(15, 'Nandita', 'dhyanand1132@gmail.com', '$2y$10$WtDw49eVTZD7GMhl5wEFAuLQ.q.Hbbvme55dodf79gCsy1BjNA0iC', 0, 'verified'),
-(16, 'Shweta', 'panchalshweta312@gmail.com', '$2y$10$GwrndCb1a1x6LyOLE6pmLu3aWMVMh5JCgo5YFL1P/B.En94hPULcC', 0, 'verified');
+INSERT INTO `usertable` (`id`, `name`, `email`, `contact`, `address`, `city`, `photo`, `password`, `code`, `status`) VALUES
+(1, 'admin', 'admin.servify.24x7@gmail.com', '', '', '', '', 'admin123', 481913, 'verified'),
+(5, 'Paritosh', 'shivgaming007@gmail.com', '', '', '', '', '$2y$10$h1K1kECGvmtIKRsJv6Ucx.3RUjUEdMkd1l/t5WbWtf58pBAH5Vc0i', 0, 'verified'),
+(6, 'Suraj Mehta', 'mehtasuraj7984@gmail.com', '', '', '', '', '$2y$10$sEsUilAaPchVY5/ETSWwJOvrVTEI6LtMbXbfazIqL/r44OC3N2KU.', 564458, 'verified'),
+(13, 'Tirth', 'paritoshg84@gmail.com', '', '', '', '', '$2y$10$knbNw5aY2JKsKDDWFtTwrOWYZ/5jAE.0r20OrMv0ncYfzIzfmd/pu', 0, 'verified'),
+(15, 'Nandita', 'dhyanand1132@gmail.com', '2323232323', 'Nani Kadi', 'Ahmedabad', 'profile.jpg', '$2y$10$WtDw49eVTZD7GMhl5wEFAuLQ.q.Hbbvme55dodf79gCsy1BjNA0iC', 362048, 'verified'),
+(16, 'Shweta', 'panchalshweta312@gmail.com', '', '', '', '', '$2y$10$GwrndCb1a1x6LyOLE6pmLu3aWMVMh5JCgo5YFL1P/B.En94hPULcC', 0, 'verified');
 
 --
 -- Indexes for dumped tables
